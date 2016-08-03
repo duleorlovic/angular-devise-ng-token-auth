7f0efad408a3d48e5fca531e7858fec3149e3126
Dusan Orlovic <duleorlovic@gmail.com>
2016-08-03 17:00:59 +0200

    Articles

--- client/src/app/components/articles/article.factory.js
+++ client/src/app/components/articles/article.factory.js
@@ -2,7 +2,7 @@
 
 angular.module('myappAngular')
   .factory('Article', function ($resource) {
-    return $resource('http://localhost:3004/articles/:articleId', {
+    return $resource('http://localhost:3004/articles.json/:articleId', {
       articleId: '@id'
     }, {
       update: {
